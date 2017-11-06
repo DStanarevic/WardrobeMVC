c1a58e0631cbc3807dcc9aaa2fe2eb37d7c61b5a
DStanarevic <DejanStanarevic@gmail.com>
2017-11-05 21:45:42 -0500

    Final Commit

---
+++ Wardrobe_MVC/Controllers/CombinationsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wardrobe_MVC.Models;
+
+namespace Wardrobe_MVC.Controllers
+{
+    public class CombinationsController : Controller
+    {
+        private Wardrobe_MVCContext db = new Wardrobe_MVCContext();
+
+        // GET: Combinations
+        public ActionResult Index()
+        {
+            var combinations = db.Combinations.Include(c => c.Accessory).Include(c => c.Bottom).Include(c => c.Shoe).Include(c => c.Top);
+            return View(combinations.ToList());
+        }
+
+        // GET: Combinations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Combination combination = db.Combinations.Find(id);
+            if (combination == null)
+            {
+                return HttpNotFound();
+            }
+            return View(combination);
+        }
+
+        // GET: Combinations/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "Name");
+            ViewBag.BottomID = new SelectList(db.Bottoms, "BottomID", "Name");
+            ViewBag.ShoeID = new SelectList(db.Shoes, "ShoeID", "Name");
+            ViewBag.TopID = new SelectList(db.Tops, "TopID", "Name");
+            return View();
+        }
+
+        // POST: Combinations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CombinationID,TopID,BottomID,ShoeID,AccessoryID")] Combination combination)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Combinations.Add(combination);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "Name", combination.AccessoryID);
+            ViewBag.BottomID = new SelectList(db.Bottoms, "BottomID", "Name", combination.BottomID);
+            ViewBag.ShoeID = new SelectList(db.Shoes, "ShoeID", "Name", combination.ShoeID);
+            ViewBag.TopID = new SelectList(db.Tops, "TopID", "Name", combination.TopID);
+            return View(combination);
+        }
+
+        // GET: Combinations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Combination combination = db.Combinations.Find(id);
+            if (combination == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "Name", combination.AccessoryID);
+            ViewBag.BottomID = new SelectList(db.Bottoms, "BottomID", "Name", combination.BottomID);
+            ViewBag.ShoeID = new SelectList(db.Shoes, "ShoeID", "Name", combination.ShoeID);
+            ViewBag.TopID = new SelectList(db.Tops, "TopID", "Name", combination.TopID);
+            return View(combination);
+        }
+
+        // POST: Combinations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CombinationID,TopID,BottomID,ShoeID,AccessoryID")] Combination combination)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(combination).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "Name", combination.AccessoryID);
+            ViewBag.BottomID = new SelectList(db.Bottoms, "BottomID", "Name", combination.BottomID);
+            ViewBag.ShoeID = new SelectList(db.Shoes, "ShoeID", "Name", combination.ShoeID);
+            ViewBag.TopID = new SelectList(db.Tops, "TopID", "Name", combination.TopID);
+            return View(combination);
+        }
+
+        // GET: Combinations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Combination combination = db.Combinations.Find(id);
+            if (combination == null)
+            {
+                return HttpNotFound();
+            }
+            return View(combination);
+        }
+
+        // POST: Combinations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Combination combination = db.Combinations.Find(id);
+            db.Combinations.Remove(combination);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Wardrobe_MVC/Models/Wardrobe_MVCContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Wardrobe_MVC.Models
+{
+    public class Wardrobe_MVCContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Wardrobe_MVCContext() : base("name=Wardrobe_MVCContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Wardrobe_MVC.Models.Top> Tops { get; set; }
+
+        public System.Data.Entity.DbSet<Wardrobe_MVC.Models.Bottom> Bottoms { get; set; }
+
+        public System.Data.Entity.DbSet<Wardrobe_MVC.Models.Shoe> Shoes { get; set; }
+
+        public System.Data.Entity.DbSet<Wardrobe_MVC.Models.Accessory> Accessories { get; set; }
+
+        public System.Data.Entity.DbSet<Wardrobe_MVC.Models.Combination> Combinations { get; set; }
+    }
+}
